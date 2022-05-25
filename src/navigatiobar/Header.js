cae306862df8528b6c8064ab849962b7f69668ba
Chinchu Joshy <chinchu.joshy@ftsindia.in>
2022-05-25 18:26:29 +0530

    first commit

---
+++ src/navigatiobar/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='head-main'> 
+    <div className="head-left">
+   <i >Icon</i>
+   <img src="./public/images/1.png" alt="" className="brand" />
+   <h3 className="brand">DIGITAL WIND</h3>
+    </div>
+    <div className="head-middle">
+    <ul>
+        <li>
+            STATUS
+        </li>
+        <li>ANALYSIS</li>
+        <li>PREDICTION</li>
+    </ul>
+    </div>
+    <div className="head-right">
+<ul>
+    <li>
+
+    </li>
+    <li>
+
+    </li>
+    <li>
+
+    </li>
+</ul>
+    </div>
+    
+    </div>
+  )
+}
+
+export default Header
---
+++ src/threejs/Threescene.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react'
+import * as THREE from 'three'
+
+function Threescene() {
+  const mountRef = useRef(null)
+  useEffect(() => {
+    const innerThreeRef=mountRef.current
+   const scene=new THREE.Scene();
+   scene.background = new THREE.Color( 0x03293F );
+   const renderer=new THREE.WebGL1Renderer()
+   renderer.setSize(window.innerWidth,window.innerHeight)
+   innerThreeRef.appendChild(renderer.domElement)
+   const camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1)
+    camera.position.z=5
+    renderer.render(scene,camera)
+    // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    // var cube = new THREE.Mesh( geometry, material );
+    
+    // scene.add( cube );
+    // var animate = function () {
+    //   requestAnimationFrame( animate );
+    //   cube.rotation.x += 0.01;
+    //   cube.rotation.y += 0.01;
+    //   renderer.render( scene, camera );
+    // };
+    
+    // animate();
+  }, [])
+  
+  return (
+    <div ref={mountRef}></div>
+  )
+}
+
+export default Threescene
